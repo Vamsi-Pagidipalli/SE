91fe865bdb5d54b0d732455b510356ee0e09951b
Vamsi-Pagidipalli <vamsipagidipalli63397@gmail.com>
2024-11-11 16:31:55 -0400

    N0-ties feature implementation

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -13,7 +13,9 @@ def get_computer_choice():
 
 # Function to determine the winner
 def determine_winner(user_choice, computer_choice):
-    if (user_choice == 'rock' and computer_choice == 'scissors') or \
+    if user_choice == computer_choice:
+        return "tie"
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
          (user_choice == 'scissors' and computer_choice == 'paper') or \
          (user_choice == 'paper' and computer_choice == 'rock'):
         return "user"
@@ -32,7 +34,9 @@ def play_game():
         print(f"Computer chose: {computer_choice}")
         
         winner = determine_winner(user_choice, computer_choice)
-        if winner == "user":
+        if winner == "tie":
+            print("It's a tie! Rematching...\n")
+        elif winner == "user":
             print("Congratulations! You win!")
             break
         else:
