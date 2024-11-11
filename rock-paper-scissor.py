e0df6e83b204a18da758a802f9c5278d2b6aafac
Vamsi-Pagidipalli <vamsipagidipalli63397@gmail.com>
2024-11-11 16:26:35 -0400

    User Greetings

---
+++ rock-paper-scissor.py
@@ -0,0 +1,13 @@
+import random
+
+# Function to greet the user
+def greet_user():
+    print("Welcome to Rock-Paper-Scissors!")
+
+# Main function to play the game
+def play_game():
+    greet_user()
+
+
+if __name__ == "__main__":
+    play_game()
